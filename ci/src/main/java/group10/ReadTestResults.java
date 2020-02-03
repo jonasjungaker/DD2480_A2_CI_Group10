f6e2c83097c04675d9aef67bbab42abe85374b2d
Olivia Stigeborn <osti@kth.se>
2020-02-03 15:25:59 +0100

    feat: Add method for converting test results to JSON format #22

--- ci/src/main/java/group10/ReadTestResults.java
+++ ci/src/main/java/group10/ReadTestResults.java
@@ -1,46 +1,82 @@
 package group10;
 
 import java.io.File;
-import java.io.IOException;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import group10.util.Path;
-
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
 
 public class ReadTestResults {
     private Path p;
-    SAXBuilder saxBuilder;
 
     public ReadTestResults() {
         p = new Path();
-        saxBuilder = new SAXBuilder();
     }
     
-    public JSONObject read() {
-        File reportDirectory = p.fileDFS("/dump/", "surefire-reports");
-        SAXBuilder saxBuilder = new SAXBuilder();
+    /**
+     * Read the XMl files generated after runnings tests and 
+     * return the relevant information as a JSONObject
+     * @param fileName the file name to the result xml files from tests
+     * @param root the root of the file with results
+     * @return jsonobject with info from tests
+     * @throws ParserConfigurationException
+     */
+    public JSONObject read(String root, String fileName) throws ParserConfigurationException {
+        File reportDirectory = p.fileDFS(root, fileName);
 
+        //Prepare JSONObject to return
+        JSONObject json = new JSONObject();
+        boolean success = false;
+        int number_failed = 0;
+        int number_success = 0;
+        JSONArray failed = new JSONArray();
+        
+        //Get the values needed from the xml file
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
         File[] reports = reportDirectory.listFiles();
         for (File report : reports) {
             if (report.getName().startsWith("TEST")) {
                 try {
-                    Document document = saxBuilder.build(report);   
+                    Document document = builder.parse(report);
+                    document.getDocumentElement().normalize();
+                    NodeList nodeTestCases = document.getElementsByTagName("testcase");
+                    for(int i = 0; i < nodeTestCases.getLength(); i++){
+                        Node n = nodeTestCases.item(i);
+                        Node child = n.getFirstChild();
+                        if(child != null){
+                            number_failed++;
+                            JSONObject failedTest = new JSONObject();
+                            failedTest.put("name", nodeTestCases.item(i).getAttributes().getNamedItem("name").getTextContent());
+                            failedTest.put("test_number", i);
+                            failedTest.put("cause", nodeTestCases.item(i).getTextContent());
+                            failed.put(failedTest);
+                        }else{
+                            number_success++;
+                        }
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+        
+        //Fill the json with the results
+        if(number_failed == 0) success = true;
+        json.put("success", success);
+        json.put("number_failed",number_failed);
+        json.put("number_success", number_success);
+        json.put("failed", failed);
 
-        return new JSONObject();
+        return json;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException {
         ReadTestResults rtr = new ReadTestResults();
-        rtr.read();
+        JSONObject json = rtr.read("/ci/target/", "surefire-reports");
     }
 
 }
