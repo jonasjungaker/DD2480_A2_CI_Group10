8238e73be8949d4902d1c24f400724cb07e1f477
Olivia Stigeborn <osti@kth.se>
2020-02-03 09:23:17 +0100

    feat(Clone): Add method and tests for cloning a repo #7

---
+++ ci/src/test/java/group10/CloneTest.java
@@ -0,0 +1,57 @@
+package group10;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test cloning a repository
+ */
+public class CloneTest {
+
+  CIServer ci;
+  File cloneDirectoryPath;
+
+  @Before
+  public void setUp() {
+    ci = new CIServer();
+    cloneDirectoryPath = new File("../../gitclones");
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    ci.tearDown(cloneDirectoryPath);
+  }
+
+  /**
+   * Tests cloning a repository where the repo and branch exist
+   * Should be true when checking if directory exist
+   * @throws IOException
+   */
+  @Test
+  public void cloneTest() throws IOException
+    {
+        String repoUrl = "https://github.com/jonasjungaker/DD2480_A2_CI_Group10";
+        ci.cloneRepository(repoUrl, "master", cloneDirectoryPath);
+        assertTrue(cloneDirectoryPath.exists());
+    }
+
+  /**
+   * Tests cloning a branch that does not exist
+   * Directory should not exist and false should be returned
+   * @throws IOException
+   */
+  @Test
+  public void branchNotExistTest() throws IOException
+    {
+        String repoUrl = "https://github.com/jonasjungaker/DD2480_A2_CI_Group10";
+        ci.cloneRepository(repoUrl, "aNonExistingBranch", cloneDirectoryPath);
+        assertFalse(cloneDirectoryPath.exists());
+    }
+}
