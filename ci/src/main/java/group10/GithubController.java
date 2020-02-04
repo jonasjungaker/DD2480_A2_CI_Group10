7b4cea2062c1dbd236e4434ce6f75007fbbbdc3f
Olivia Stigeborn <osti@kth.se>
2020-02-04 13:15:51 +0100

    fix: log exceptions in handlePost to avoid web server crash #42

--- ci/src/main/java/group10/GithubController.java
+++ ci/src/main/java/group10/GithubController.java
@@ -24,7 +24,7 @@ public class GithubController {
      * @throws IOException
      * @throws JSONException
      */
-    public static String handlePost(Request request, Response response) throws JSONException, IOException {
+    public static String handlePost(Request request, Response response){
         // turn data string into a map structure
         System.out.println(request.body());
         JSONObject all_data = json.toMap(request.body());   
@@ -34,14 +34,29 @@ public class GithubController {
         // todo set commit pending
         // clone repo
         File cloneDirectoryPath = new File("/clone");
-        boolean cloned = ciServer.cloneRepository(relevant_data.getString("clone_url"), relevant_data.getString("ref"), cloneDirectoryPath);
+        boolean cloned = false;
+        try {
+            cloned = ciServer.cloneRepository(relevant_data.getString("clone_url"), relevant_data.getString("ref"),
+                    cloneDirectoryPath);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            System.out.println("Failed cloning with: "+ e.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed cloning with: "+ e.getMessage());
+        }
         
         // compile repo
         // run tests
         // set commit
 
         //tear down the session
-        ciServer.tearDown(cloneDirectoryPath);
+        try {
+            ciServer.tearDown(cloneDirectoryPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed tearDown "+ e.getMessage());
+        }
 
         //Check that everything went as it should
         if(cloned){
