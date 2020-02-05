163240cfd17b3cb0f571a1c0d5eb8b250e89d645
Olivia Stigeborn <osti@kth.se>
2020-02-05 13:15:30 +0100

    refactor(Clone): Moved clone function to GithubController #48

--- ci/src/main/java/group10/CIServer.java
+++ ci/src/main/java/group10/CIServer.java
@@ -5,22 +5,12 @@ import static spark.Spark.*;
 import group10.util.Path;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.CloneCommand;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.json.JSONObject;
 
 public class CIServer {
-
-    Git git;
-    File cloneDirectoryPath;
 
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
@@ -57,52 +47,6 @@ public class CIServer {
             return new JSONObject();
         } else  {
             return new JSONObject(data);
-        }
-    }
-    
-     /**
-     * Clone the given repository & branch from git to the specified file
-     * @param url the url of the repository to clone
-     * @param branch the branch to clone
-     * @param cloneDirectoryPath the file to clone the repo to
-     * @return true if cloning was successful
-     */
-    public boolean cloneRepository(String url, String branch, File cloneDirectoryPath) throws IOException {
-        // Set which branch to clone
-        ArrayList<String> branches = new ArrayList<>();
-        String branchPath = branch;
-        branches.add(branchPath);
-
-        // Clone the repository
-        try {
-            CloneCommand gitclone = new CloneCommand();
-            gitclone.setURI(url);
-            gitclone.setDirectory(cloneDirectoryPath);
-            gitclone.setBranchesToClone(branches);
-            gitclone.setBranch(branch);
-            git = gitclone.call();
-            return true;
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-        
-
-    /**
-     * Teardown session to prepare for next execution. 
-     * Close the git session
-     * Delete the cloned repo
-     * @param file the cloned repo
-     * @throws IOException
-     */
-    public void tearDown(File file) throws IOException {
-        if(git != null){
-            git.close();
-            git = null;
-        }
-        if(file.exists()){
-            FileUtils.deleteDirectory(file);
         }
     }
 }
