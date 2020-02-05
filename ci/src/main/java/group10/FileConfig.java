10f95d08d698c6c699e8394e5614cb46b45945f4
Johan von Hacht <johvh@kth.se>
2020-02-05 17:28:35 +0100

    feat: set commit status #5 (#65)
    * feat(respond) added a post-request function #5
    
    * added method description #5
    
    * refactor: removed the post function from CIServer.java #5
    
    * Added the a class FileConfig to extract data from files #5
    
    * the post function is now using FileConfig methods #5
    
    Co-authored-by: Diego Leon <diegoflores4711@hotmail.com>
    Co-authored-by: Olivia Stigeborn <Olivia.stigeborn@tink.se>

---
+++ ci/src/main/java/group10/FileConfig.java
@@ -0,0 +1,48 @@
+package group10;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.NoSuchElementException;
+
+public class FileConfig {
+    /**
+     * Method that returns the element from a specific row in the config file
+     * 
+     * @param test is test name
+     * @return element on the row
+     */
+    public static String getRow(int row){
+        String line = "";
+        String path = getPath();
+        int i = -1;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path+"config.txt"));
+            
+            while ((line = br.readLine()) != null && i < row) {
+                if(i == row-1) {
+                    return line;
+                }
+                i++;
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        throw new IndexOutOfBoundsException("Row out of bounds");
+    }
+
+    /**
+     * Method that returns the path to the config file depending on the OS
+     * 
+     * @return the path is returned
+     */
+    public static String getPath() {
+        String os = System.getProperty("os.name").toLowerCase();
+        String user = System.getProperty("user.name");
+        if(os.indexOf("win") >= 0) {
+            return "C:"+"\\Users\\"+user+"\\"+"Documents";
+        } else if(os.indexOf("mac") >= 0) {
+            return "/Users/"+user+"/Sites/";
+        }
+
+        throw new NoSuchElementException("Could not find file");
+    }
+}
--- ci/src/main/java/group10/GithubController.java
+++ ci/src/main/java/group10/GithubController.java
@@ -16,9 +16,17 @@ import org.json.JSONObject;
 
 import group10.util.JsonUtil;
 
+import group10.FileConfig;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.util.Base64;
+import java.net.URL;
+
 public class GithubController {
     private static JsonUtil json = new JsonUtil();
-    private static CIServer ciServer = new CIServer();
     static Git git;
     File cloneDirectoryPath;
 
@@ -34,10 +42,10 @@ public class GithubController {
     public static String handlePost(Request request, Response response){
         // turn data string into a map structure
         System.out.println(request.body());
-        JSONObject all_data = json.toMap(request.body());   
+        JSONObject all_data = json.toMap(request.body());
         JSONObject relevant_data = json.getRelevantData(all_data);
         System.out.println(relevant_data.toString());
-        
+
         // todo set commit pending
         // clone repo
         File cloneDirectoryPath = new File("clone");
@@ -56,6 +64,7 @@ public class GithubController {
         // compile repo
         // run tests
         // set commit
+        setCommitStatus("test", "success", relevant_data.get("sha").toString());
 
         //tear down the session
         try {
@@ -71,6 +80,44 @@ public class GithubController {
         }
         return "failed";
     };
+
+
+    /**
+     * Method that sends pass and fail for the build results.
+     * 
+     * @param test is test name
+     * @param state is the test result 
+     * @param sha is the commit id
+     */
+    public static void setCommitStatus(String test, String state, String sha) {
+        String user = FileConfig.getRow(0);
+        String token = FileConfig.getRow(1);
+        String repo = FileConfig.getRow(2);
+        String url = "https://api.github.com/repos/" + user + "/" + repo + "/statuses/" + sha;
+        String json = "{\"state\": \"" + state + "\", \"description\": \"desc\", \"context\": \"" + test
+                + "\", \"target_url\": \"http://dleon.johvh.se\"}";
+        
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty ("Authorization", "Basic " + Base64.getEncoder().encodeToString((user+":"+token).getBytes()));
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(json);
+            wr.flush();
+            wr.close();
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            in.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+    }
+
+
+
 
      /**
      * Clone the given repository & branch from git to the specified file
