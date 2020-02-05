e576ce3cc35d8c07c3a606663ffaf07113e6f4e0
Emma Olsson <emmaolss@kth.se>
2020-02-05 15:03:49 +0100

    feat: Make working template for dynamic data input #37

--- ci/src/main/java/group10/WebsiteController.java
+++ ci/src/main/java/group10/WebsiteController.java
@@ -6,6 +6,9 @@ import spark.Response;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.json.JSONArray;
 
 public class WebsiteController {
@@ -13,18 +16,20 @@ public class WebsiteController {
     /**
      * Handle the Website get event: get info about build history from database
      * return response.
-     * @param request from website to server
+     * 
+     * @param request  from website to server
      * @param response send back info from database
      * @return
      */
-    public static String handleGet(Request request, Response response) 
-    {
-        JSONArray model = new JSONArray();
-        //todo: get data from DB
-
-        JSONArray builds = new JSONArray("[{\"date\":\"2020-02-05\",\"elapsed\":0,\"number_failed\":0,\"author\":\"pepe\",\"buildID\":70,\"number_passed\":0,\"branch\":\"cool\",\"status\":\"pending\"},{\"date\":\"2020-02-05\",\"elapsed\":0,\"number_failed\":0,\"author\":\"pepega\",\"buildID\":71,\"number_passed\":0,\"branch\":\"korv\",\"status\":\"pending\"}]");
-        model.put(builds);
-        return new VelocityTemplateEngine().render(new ModelAndView(model, "public/exampleTemplate.html"));
+    public static String handleGet(Request request, Response response) {
+        //JSONArray model = new JSONArray();
+        // todo: get data from DB
+        
+        JSONArray builds = new JSONArray("[{\"date\":\"2020-02-05\",\"elapsed\":0,\"number_failed\":0,\"author\":\"pepe\",\"buildID\":70,\"number_passed\":0,\"branch\":\"cool\",\"status\":\"pending\"}]");
+        Map<String, Object> buildModel = new HashMap<>();
+        buildModel.put("builds", builds);
+        
+        return new VelocityTemplateEngine().render(new ModelAndView(buildModel, "public/exampleTemplate.vm"));
     } 
     
     public static String render(JSONArray model, String templatePath) {
