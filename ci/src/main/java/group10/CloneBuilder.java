11ed33ea6e16b7a8bbca2cc82f0b303277d9ac31
Olivia Stigeborn <osti@kth.se>
2020-02-06 14:16:16 +0100

    docs: merge with master #11

--- ci/src/main/java/group10/CloneBuilder.java
+++ ci/src/main/java/group10/CloneBuilder.java
@@ -1,12 +1,9 @@
 package group10;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-import org.apache.maven.cli.MavenCli;
 
 public class CloneBuilder {
     public String path;
@@ -27,15 +24,9 @@ public class CloneBuilder {
     }
 
     /**
-<<<<<<< HEAD
-     * Builds the project and creates a buildoutput at this.buildOutput Also updates
-     * the builddata of the build in order to see if the build and tests were
-     * successful
-=======
      * Builds the project and creates a buildoutput at this.buildOutput
      * Also updates the builddata of the build in order to see if the build and tests were successful
      * @return true if rebuild succeded false otherwise
->>>>>>> docs: fixed javadoc errors #11
      */
     public boolean rebuild() {
         this.buildOutput = this.build();
@@ -45,15 +36,10 @@ public class CloneBuilder {
     }
 
     /**
-<<<<<<< HEAD
-     * builds a maven project at the designated path
+     * Builds a maven project at the designated path
      * 
      * @return a string of the output of the build process for checking with the
      *         checkBuildOutput method
-=======
-     * Builds a maven project at the designated path
-     * @return a string of the output of the build process for checking with the checkBuildOutput method
->>>>>>> docs: fixed javadoc errors #11
      */
     private String build() {
 
