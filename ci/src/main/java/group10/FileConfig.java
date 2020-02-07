280c56320fe49745d6759de142d834e613804e17
Johan von Hacht <johvh@kth.se>
2020-02-07 11:24:10 +0100

    fix: change config file to root location #101 (#103)

--- ci/src/main/java/group10/FileConfig.java
+++ ci/src/main/java/group10/FileConfig.java
@@ -11,17 +11,20 @@ public class FileConfig {
      */
     public static String getRow(int row) {
         String line = "";
-        String path = System.getProperty("user.dir") + "/../../";
+        String path = System.getProperty("user.home") + "/ci/";
         int i = -1;
         try {
-            BufferedReader br = new BufferedReader(new FileReader(path+"config.txt"));
+            FileReader fr = new FileReader(path + "config.txt");
+            BufferedReader br = new BufferedReader(fr);
             
             while ((line = br.readLine()) != null && i < row) {
                 if(i == row-1) {
+                    br.close();
                     return line;
                 }
                 i++;
             }
+            br.close();
         } catch(Exception e) {
             e.printStackTrace();
         }
